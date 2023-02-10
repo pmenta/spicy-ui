50952e80a0dc575874f66071454e22634134e2b5
João Martins <jvmartinsdev@gmail.com>
2023-02-10 19:27:17 -0300

    v1

---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,109 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  transition: 'opacity 0.2s',
+
+  '&:not(:disabled):hover': {
+    opacity: 0.8,
+  },
+
+  '&:not(:disabled):active': {
+    opacity: 0.6,
+  },
+
+  '&:not(:disabled):focus': {
+    outline: '1.5px solid $gray900',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$spicy500',
+
+        '&:not(:disabled):hover': {
+          background: '$spicy300',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
+      },
+
+      secondary: {
+        color: '$spicy300',
+        border: '2px solid $spicy500',
+
+        '&:not(:disabled):hover': {
+          background: '$spicy500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
+
+Button.displayName = 'Button'
---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  spicy300: '#F24141',
+  spicy500: '#E53E3E',
+  spicy600: '#C53030',
+  spicy700: '#9B2C2C',
+  spicy900: '#822424',
+
+  blue500: '#3182CE',
+
+  cyan300: '#76E5F7',
+}
